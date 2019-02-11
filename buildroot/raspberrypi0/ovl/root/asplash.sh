aa2e7b1a8c45d7ab6983ff0c2ecf376ac5869630
Hien TRAN-QUANG <hien.tranquang@gmail.com>
2019-02-12 00:36:20 +0100

    Revert "Ajout configurations Buildroot et fichiers Overlay"
    This reverts commit 848d791ce656e499ebbfec54afcf24988a95c2b2.

--- buildroot/raspberrypi0/ovl/root/asplash.sh
+++ buildroot/raspberrypi0/ovl/root/asplash.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dd if=/root/logo_paysage.fb of=/dev/fb0 bs=1536000 count=1 > /dev/null 2>&1
+dd if=/root/logo.fb of=/dev/fb0 bs=1536000 count=1 > /dev/null 2>&1
--- buildroot/raspberrypi0/ovl/root/asplash_paysage.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-dd if=/root/logo_paysage.fb of=/dev/fb0 bs=1536000 count=1 > /dev/null 2>&1
