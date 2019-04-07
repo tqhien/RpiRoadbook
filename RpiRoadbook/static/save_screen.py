aee01f14756a47a5bcac97f0ef41e6057c72fc0d
Hien TRAN-QUANG <hien.tranquang@gmail.com>
2019-04-07 19:18:36 +0200

    HTML : ajout de la configuration des écrans en mode Rallye
    Interface HTML pour personnaliser ses 3 écrans d'affichage en mode rallye.

--- RpiRoadbook/static/save_screen.py
+++ RpiRoadbook/static/save_screen.py
@@ -9,20 +9,40 @@ import configparser
 screenconfig = configparser.ConfigParser()
 
 # On charge les reglages :
-candidates = ['/home/rpi/RpiRoadbook/screen.cfg','/mnt/piusb/.conf/RpiRoadbook_screen.cfg']
+candidates = ['/home/hien/Developpement/RpiRoadbook/RpiRoadbook/screen.cfg','/home/rpi/RpiRoadbook/screen.cfg','/mnt/piusb/.conf/RpiRoadbook_screen.cfg']
 screenconfig.read(candidates)
 
+print("Content-Type: text/html")
 print("""
-Content-Type: text/html
 <html>
+<head>
+  <link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
 <body>
+<div id="main">
+<h1>Configuration des &eacutecrans</h1>
+<hr>
 """)
 
-for i in range (1,6) :
-    for j in range (1,4) :
-        fji = 'f{}{}'.format(j,i)
-        if fji in form:
-            screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] = form[fij].value
+for k in range (1,4) :
+    jour_nuit = 'jour_nuit{}'.format(k)
+    if jour_nuit in form:
+        screenconfig['Affichage{}'.format(k)]['jour_nuit'] = form[jour_nuit].value
+    luminosite = 'luminosite{}'.format(k)
+    if luminosite in form:
+        v = int(form[luminosite].value)
+        if v > 100 :
+            v = 100
+        if v < 5 :
+            v = 5
+        screenconfig['Affichage{}'.format(k)]['luminosite'] = v
+    layout = 'layout{}'.format(k)
+    if layout in form:
+        screenconfig['Affichage{}'.format(k)]['layout'] = form[layout].value
+    for j in range (1,7) :
+        champkj = 'champ{}{}'.format(k,j)
+        if champkj in form:
+            screenconfig['Affichage{}'.format(k)]['ligne{}'.format(j)] = form[champkj].value
 
 for attempt in range(5):
   try :
@@ -37,5 +57,7 @@ else :
   print('Write Error RpiRoadbook_screen.cfg after 5 tries')
 
 print ("""
+<h3>Configuration sauvegard&eacute; !</h3>
+<hr>
 <a href="index.py"> <input type="button" value="Accueil"> </a>
-</body></html>""")
+</div></body></html>""")
--- RpiRoadbook/static/screen_setup.py
+++ RpiRoadbook/static/screen_setup.py
@@ -3,50 +3,160 @@
 import cgi, os
 import cgitb; cgitb.enable()
 import configparser
+import time
 
 screenconfig = configparser.ConfigParser()
+setupconfig = configparser.ConfigParser()
 
-# On charge les reglages : 
-candidates = ['/home/rpi/RpiRoadbook/screen.cfg','/mnt/piusb/.conf/RpiRoadbook_screen.cfg']
+# On charge les reglages : mode, orientation, etc
+candidates = ['/home/hien/Developpement/RpiRoadbook/RpiRoadbook/setup.cfg','/home/rpi/RpiRoadbook/setup.cfg','/mnt/piusb/.conf/RpiRoadbook_setup.cfg']
+setupconfig.read(candidates)
+
+candidates = ['/home/hien/Developpement/RpiRoadbook/RpiRoadbook/screen.cfg','/home/rpi/RpiRoadbook/screen.cfg','/mnt/piusb/.conf/RpiRoadbook_screen.cfg']
 screenconfig.read(candidates)
+
+orientation = setupconfig['Parametres']['orientation']
+if orientation == 'Paysage' :
+    st = 'pa'
+    nb_lignes = [3,4,5,6,3,3]
+else:
+    st = 'po'
+    nb_lignes = [2,3,4,1,2,3]
 
 print("""
 <html>
-<body>
-   <form action = "save_screen.py" method = "post">
+  <head>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <script type="text/javascript">
+     function update_preview()
+        {
+        var st = ['','','','']; """)
+print('var nb_lignes = {};'.format(nb_lignes))
+print('for (k=1;k<4;k++) {')
+print("            st[k]='{}' ;".format(st))
+print("""            var b = document.getElementById("jour_nuit"+k)[document.getElementById("jour_nuit"+k).selectedIndex].value
+            switch (b)
+            {
+            case "Jour":
+                st[k] = st[k] + 'j';
+                break;
+            case "Nuit":
+                st[k] = st[k] + 'n';
+                break;
+            }
 
-<table>
-    <tr>
-        <td></td>
-        <td>Affichage 1</td>
-        <td>Affichage 2</td>
-        <td>Affichage 3</td>
-    </tr>
-    <tr>""")
+            st[k] = st[k] + 'ra';
 
-for i in range (1,6) :
+            var d = document.getElementById("layout"+k)[document.getElementById("layout"+k).selectedIndex].value
+            st[k] = st[k] + d
+
+            //document.getElementById("preview").innerHTML = '<h1>'+st+'</h1>';
+          	document.getElementById("preview_img"+k).src = 'images/'+st[k] + '.png';
+          var j = document.getElementById("layout"+k).selectedIndex;
+          var nb_l = nb_lignes[j]
+          var i;
+          for (i = 1;i<=nb_l;i++){
+          	document.getElementById("champ"+k+i).style.display = "inline"
+            document.getElementById("champ"+k+i).disabled = false;
+          };
+          for (i = nb_l+1;i<7;i++){
+          	document.getElementById("champ"+k+i).style.display = "none";
+            document.getElementById("champ"+k+i).disabled = true;
+          }
+          }
+
+
+            //change_mode(listindex)
+
+            return true;
+        }
+
+</script> </head>
+  <body onload=update_preview()>
+  <div id="main">
+    <h1>Configuration des &eacutecrans</h1>
+    <hr>
+    <form action="save_screen.py" method="post">
+        <table>
+        <tr><th></th><th>Ecran 1</th><th>Ecran 2</th><th>Ecran 3</th></tr>
+""")
+print('<tr><td>Preview</td>')
+for j in range (1,4):
+    # Affichage des previews
+    print('<td>')
+    print('<img src="images/pajra1.png" id="preview_img{}">'.format(j))
+    print('</td>')
+print('</tr><tr></tr>')
+
+# Affichage des listes de choix jour_nuit
+print('<tr><td>Mode</td>')
+for j in range (1,4):
+    print('<td>')
+    print('<select name="jour_nuit" id="jour_nuit{}" onchange="update_preview()" style="width:110px;">'.format(j))
+    print('<option value="Jour" selected="Jour">Jour</option>' if screenconfig['Affichage{}'.format(j)]['jour_nuit'] == 'Jour' else '<option value="Jour">Jour</option>')
+    print('<option value="Nuit" selected="Nuit">Nuit</option>' if screenconfig['Affichage{}'.format(j)]['jour_nuit'] == 'Nuit' else '<option value="Nuit">Nuit</option>')
+    print('</select>')
+    print('</td>')
+print('</tr>')
+
+# Affichage des choix de luminosite
+print('<tr><td>Luminosit&eacute;</td>')
+for j in range (1,4):
+    print('<td>')
+    print('<input type="number" id="luminosite{}" name="luminosite" value="{}" style="width:110px;"> %'.format(j,screenconfig['Affichage{}'.format(j)]['luminosite']))
+    print('</td>')
+print('</tr>')
+
+# Affichage des listes de layout
+print('<tr><td>Disposition</td>')
+for j in range (1,4):
+
+    print('<td>')
+    print('<select name="layout" id="layout{}" onchange="update_preview()" style="width:110px;">'.format(j))
+    if orientation == 'Paysage' :
+        print('<option selected="1" value="1">3 Champs</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '1' else '<option value="1">3 Champs</option>')
+        print('<option selected="2" value="2">4 Champs</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '2' else '<option value="2">4 Champs</option>')
+        print('<option selected="3" value="3">5 Champs</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '3' else '<option value="3">5 Champs</option>')
+        print('<option selected="4" value="4">6 Champs</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '4' else '<option value="4">6 Champs</option>')
+    else :
+        print('<option selected="1" value="1">Layout 1</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '1' else '<option value="1">Layout 1</option>')
+        print('<option selected="2" value="2">Layout 2</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '2' else '<option value="2">Layout 2</option>')
+        print('<option selected="3" value="3">Layout 3</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '3' else '<option value="3">Layout 3</option>')
+        print('<option selected="4" value="4">Layout 4</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '4' else '<option value="4">Layout 4</option>')
+        print('<option selected="5" value="5">Layout 5</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '5' else '<option value="5">Layout 5</option>')
+        print('<option selected="6" value="6">Layout 6</option>' if screenconfig['Affichage{}'.format(j)]['layout'] == '6' else '<option value="6">Layout 6</option>')
+
+    print('</select>')
+    print('</td>')
+print('</tr><tr></tr>')
+for i in range (1,7) :
     print('<tr>')
-    print('<td>Ligne {} : </td>'.format(i))
+    print('<td>Widget {} : </td>'.format(i))
     for j in range (1,4) :
         print('<td>')
-        print('<select id="f{}{}" name="f{}{}">'.format(j,i,j,i))
-        print('<option selected=""></option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == '' else '<option></option>')
-        print('<option selected="Heure">Heure</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Heure' else '<option>Heure</option>')
-        print('<option selected="Chrono">Chrono</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Chrono' else '<option>Chrono</option>')
-        print('<option selected="Decompte">Decompte</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Decompte' else '<option>Decompte</option>')
-        print('<option selected="Vitesse">Vitesse</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Vitesse' else '<option>Vitesse</option>')
-        print('<option selected="Moyenne">Moyenne</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Moyenne' else '<option>Moyenne</option>')
-        print('<option selected="Vmax">Vmax</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Vmax' else '<option>Vmax</option>')
-        print('<option selected="Totalisateur">Totalisateur</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Totalisateur' else '<option>Totalisateur</option>')
-        print('<option selected="Trip1">Trip1</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Trip1' else '<option>Trip1</option>')
-        print('<option selected="Trip2">Trip2</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Trip2' else '<option>Trip2</option>')
+        print('<select id="champ{}{}" name="champ{}{}" style="width:110px;">'.format(j,i,j,i))
+        print('<option selected="" value=""></option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == '' else '<option value=""></option>')
+        print('<option selected="Totalisateur" value="Totalisateur">Odom&egrave;tre</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Totalisateur' else '<option value="Totalisateur">Odom&egrave;tre</option>')
+        print('<option selected="Trip1" value="Trip1">Trip1</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Trip1' else '<option value="Trip1">Trip1</option>')
+        print('<option selected="Trip2" value="Trip2">Trip2</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Trip2' else '<option value="Trip2">Trip2</option>')
+        print('<option selected="Vitesse" value="Vitesse">Vitesse</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Vitesse' else '<option value="Vitesse">Vitesse</option>')
+        print('<option selected="Vmoy1" value="Vmoy1">Moyenne1</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Vmoy1' else '<option value="Vmoy1">Moyenne1</option>')
+        print('<option selected="Vmoy2" value="Vmoy2">Moyenne2</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Vmoy2' else '<option value="Vmoy2">Moyenne2</option>')
+        print('<option selected="Chrono1" value="Chrono1">Chrono1</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Chrono1' else '<option value="Chrono1">Chrono1</option>')
+        print('<option selected="Chrono2" value="Chrono2">Chrono2</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Chrono2' else '<option value="Chrono2">Chrono2</option>')
+        print('<option selected="Decompte" value="Decompte">Decompte</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Decompte' else '<option value="Decompte">Decompte</option>')
+        print('<option selected="Vmax1" value="Vmax1">Vmax1</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Vmax1' else '<option value="Vmax1">Vmax1</option>')
+        print('<option selected="Vmax2" value="Vmax2">Vmax2</option>' if screenconfig['Affichage{}'.format(j)]['ligne{}'.format(i)] == 'Vmax2' else '<option value="Vmax2">Vmax2</option>')
         print('</select>')
         print('</td>')
     print('</tr>')
-print('</table>')
 print("""
-    <input type = "submit" value = "Valider" />
-   </form>
-<a href="index.py"> <input type="button" value="Retour &agrave; l\'accueil"></a>
-</body>
-</html>""")
+        </table>
+        <hr> <input value="Valider" type="submit">
+    </form>
+    <hr>
+    <a href="index.py"> <input type="button" value="Retour &agrave; l\'accueil"></a>
+    </div>
+  </body>
+</html>
+""")
