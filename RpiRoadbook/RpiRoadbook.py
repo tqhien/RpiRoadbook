5e8881b696795a760bfd30696fbeffc55e461d99
Hien TRAN-QUANG <hien.tranquang@gmail.com>
2019-04-20 01:24:05 +0200

    Ajout widget Heure

--- RpiRoadbook/RpiRoadbook.py
+++ RpiRoadbook/RpiRoadbook.py
@@ -714,6 +714,8 @@ def widget_dispatch(st,layout,widget):
         return chrono2_widget(layout,widget)
     elif st == 'Decompte' :
         return countdown_widget(layout,widget)
+    elif st == 'Heure' :
+        return heure_widget(layout,widget)
     else :
         return rb_widget(layout,widget)
 
@@ -1146,6 +1148,22 @@ class countdown_widget (rb_widget):
         else:
             blit_text(scr,'{:02.0f}:{:02.0f} '.format(m,s),(self.x+self.x2,self.y+self.y2),self.value_font, angle)
         blit_text(scr,'.{:02.0f} '.format(ss),(self.x+self.x3,self.y+self.y3),self.unit_font,angle)
+        r = pygame.draw.rect(scr,GRIS,(self.x,self.y,self.w,self.h),3)
+        pygame.display.update(r)
+
+class heure_widget(rb_widget):
+    def __init__(self,layout='1',widget=0):
+        rb_widget.__init__(self,layout,widget)
+    def update(self):
+        self.now = time.localtime()
+    def render(self,scr):
+        global angle,chrono_time2
+        blit_text(scr,' Heure',(self.x+self.x1,self.y+self.y1), self.label_font,angle)
+        if self.selected:
+            blit_text(scr,'{:02d}:{:02d}'.format(self.now.tm_hour,self.now.tm_min),(self.x+self.x2,self.y+self.y2),self.selected_font, angle)
+        else:
+            blit_text(scr,'{:02d}:{:02d}'.format(self.now.tm_hour,self.now.tm_min),(self.x+self.x2,self.y+self.y2),self.value_font, angle)
+        blit_text(scr,'{:02.0f} '.format(self.now.tm_sec),(self.x+self.x3,self.y+self.y3),self.unit_font,angle)
         r = pygame.draw.rect(scr,GRIS,(self.x,self.y,self.w,self.h),3)
         pygame.display.update(r)
 
