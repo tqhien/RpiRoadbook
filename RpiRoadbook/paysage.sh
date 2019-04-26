e31f99ad6c53aa24d4031b19954666bb65f6fec5
Hien TRAN-QUANG <hien.tranquang@gmail.com>
2019-04-27 01:08:02 +0200

    Correction basculement orientation splashscreen / scripts bash

--- RpiRoadbook/paysage.sh
+++ RpiRoadbook/paysage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo rw
+/usr/local/bin/rw
 sleep .5
-sudo cp -f /root/asplash_paysage.sh /root/asplash.sh
+/bin/cp -f /root/asplash_paysage.sh /root/asplash.sh
 sleep .5
-sudo ro
+/usr/local/bin/ro
 sleep .5
