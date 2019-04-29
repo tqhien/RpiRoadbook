1b36e239bbe56876a002e8e5bbf246d603454dc0
Hien TRAN-QUANG <hien.tranquang@gmail.com>
2019-04-29 11:52:48 +0200

    Ajout Affichage update en cours et modifications scripts orientation

--- RpiRoadbook/paysage.sh
+++ RpiRoadbook/paysage.sh
@@ -2,6 +2,8 @@
 /usr/local/bin/rw
 sleep .5
 /bin/cp -f /root/asplash_paysage.sh /root/asplash.sh
+/bin/cp -f /root/update_l.fb /root/update.fb
+/bin/cp -f /root/update_done_l.fb /root/update_done.fb
 sleep .5
 /usr/local/bin/ro
 sleep .5
--- RpiRoadbook/portrait.sh
+++ RpiRoadbook/portrait.sh
@@ -2,6 +2,8 @@
 /usr/local/bin/rw
 sleep .5
 /bin/cp -f /root/asplash_portrait.sh /root/asplash.sh
+/bin/cp -f /root/update_p.fb /root/update.fb
+/bin/cp -f /root/update_done_p.fb /root/update.fb
 sleep .5
 /usr/local/bin/ro
 sleep .5
