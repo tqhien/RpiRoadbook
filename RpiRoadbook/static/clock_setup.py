85a8f5716fe68ba4f254a03a866c197c17cc472e
Hien TRAN-QUANG <hien.tranquang@gmail.com>
2019-05-13 00:22:08 +0200

    Correction type espace devant print

--- RpiRoadbook/static/clock_setup.py
+++ RpiRoadbook/static/clock_setup.py
@@ -13,8 +13,8 @@ datetime_now = time.localtime ()
 st_date = '{}-{:02d}-{:02d}'.format(datetime_now.tm_year,datetime_now.tm_mon,datetime_now.tm_mday)
 st_time = '{:02d}:{:02d}'.format(datetime_now.tm_hour,datetime_now.tm_min)
 
-
-print("""<html>
+print("""
+<html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="w3.css">
@@ -25,9 +25,11 @@ print("""<html>
 <!-- Entete -->
 <div class="w3-container w3-center w3-section">
 </div>
-<h1>""")
+<h1>
+""")
 print(_("R&eacute;glage de l'horloge"))
-print("""</h1>
+print("""
+</h1>
 <div class="w3-container w3-section w3-topbar w3-bottombar w3-border-grey">
 <form action = "save_clock.py" method = "post">
    <div class="w3-row-padding">
@@ -37,7 +39,7 @@ print('    <label for="date"><h3>')
 print(_('Date:'))
 print('</h3></label>')
 print('    <input type="date" id="date" name="user_date" value={}'.format(st_date))
-print ('pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}">')
+print(' pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}">')
 print('  </div>')
 
 print('   <div class="w3-half">')
@@ -45,13 +47,14 @@ print('    <label for="time"><h3>')
 print(_('Heure:'))
 print('</h3></label>')
 print('    <input type="time" id="time" name="user_time" value={}'.format(st_time))
-print('pattern="[0-9]{2}:[0-9]{2}">')
+print(' pattern="[0-9]{2}:[0-9]{2}">')
 print('  </div>')
 
 print("""
 </div>
 <div class="w3-bar">
-        <button class="w3-submit w3-btn w3-red w3-hover-teal w3-margin" type="submit">""")
+        <button class="w3-submit w3-btn w3-red w3-hover-teal w3-margin" type="submit">
+""")
 print(_('Valider'))
 print("""
 </button>
@@ -62,10 +65,13 @@ print("""
 <!-- Pied de page -->
 <div class="w3-bar w3-black">
   <a class="w3-bar-item w3-button w3-hover-blue" href="index.py"><i class="w3-xlarge fa fa-home"></i></a>
-  <a href="setup.py" class="w3-bar-item w3-button w3-hover-blue w3-right"><i class="w3-xlarge fa fa-wrench"></i>""")
- print(_('Configuration'))
- print("""</a>
+  <a href="setup.py" class="w3-bar-item w3-button w3-hover-blue w3-right"><i class="w3-xlarge fa fa-wrench"></i>
+""")
+print(_('Configuration'))
+print("""
+ </a>
 </div>
 
 </body>
-</html>""")
+</html>
+""")
